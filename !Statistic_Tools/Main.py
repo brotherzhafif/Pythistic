ba2a9f0734906c4631b885b68847fa7de031c12b
brotherzhafif <bangz1504@gmail.com>
2024-10-10 05:02:15 +0700

    Finishing Frequency Table Program

---
+++ !Statistic_Tools/Main.py
@@ -0,0 +1,10 @@
+import Frequency as fr
+import Chart
+
+# Main Control For Statistic Tools
+Range_limit = fr.data_range
+Frequency_limit = fr.data_frequency
+
+print(Range_limit)
+print(Frequency_limit)
+
