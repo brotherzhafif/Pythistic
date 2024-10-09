9dcb5e84fec24d41a594e4723928d86f587bd7e6
brotherzhafif <bangz1504@gmail.com>
2024-10-09 20:14:29 +0700

    Menambahkan Arsip Belajar Alpro Python Semester 2

---
+++ !Archive ( Praktikum-Alpro )/Tugas_Kelas/calc.py
@@ -0,0 +1,12 @@
+def luas_segitiga(alas, tinggi):
+    return 0.5 * alas * tinggi
+
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
+
+def luas_jajaran_genjang(alas, tinggi):
+    return alas * tinggi
+
+def luas_lingkaran(jari_jari):
+    import math
+    return math.pi * jari_jari * jari_jari
